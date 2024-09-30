009abef734e499421f27aee47a5abee414372c50
Christian Mundal <70058199+Mondz16@users.noreply.github.com>
2024-09-30 22:17:31 +0800

    editing chirps

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+}); 
 
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'update'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
